33bfeab387febb0727837a12edb13d0db4fde331
Muhammad Wakeel <muhammadwakeel701@gmail.com>
2025-10-02 20:36:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,20 +11,21 @@ region_map = {
 }
 
 Soil_map = {
-    "Sandy":1,
-    "Clay":2,
-    "Loam":3,
-    "Silt":4,
-    "Peaty":5,
-    "Chalky":6
+    'Sandy(ریتیلی)':1, 
+    'Clay(چکنی)':2, 
+    'Loam(میرا / بھل)':3, 
+    'Silt(دھوئی ہوئی / گاد آلود)':4, 
+    'Peaty(پیٹ دار / دلدلی)':5, 
+    'Chalky(چونے دار)':6
+    
 }
 Crop_map = {
-    'Cotton':1,
-    'Rice':2,
-    'Barley':3,
-    'Soybean':4,
-    'Wheat':5,
-    'Maize':6
+    'Cotton(روئی)':1, 
+    'Rice(چاول)':2, 
+    'Barley(جو)':3, 
+    'Soybean(سویابین)':4, 
+    'Wheat(گندم)':5, 
+    'Maize(مکئی)':6
 }
 Whether_map = {
     'Cloudy':1,
@@ -38,8 +39,20 @@ with open("Model.pkl", 'rb') as f:
 st.title("Farmer's Helping Bot")
 
 Region = st.selectbox("Select Your Region:", ['East', 'West', 'North','South'])
-Soil_Type = st.selectbox("Select Your Soil Type:", ['Sandy', 'Clay', 'Loam', 'Silt', 'Peaty', 'Chalky'])
-Crop = st.selectbox("Select Your Crop:", ['Cotton', 'Rice', 'Barley', 'Soybean', 'Wheat', 'Maize'])
+Soil_Type = st.selectbox("Select Your Soil Type:", ['Sandy(ریتیلی)', 
+ 'Clay(چکنی)', 
+ 'Loam(میرا / بھل)', 
+ 'Silt(دھوئی ہوئی / گاد آلود)', 
+ 'Peaty(پیٹ دار / دلدلی)', 
+ 'Chalky(چونے دار)']
+)
+Crop = st.selectbox("Select Your Crop:", ['Cotton(روئی)', 
+ 'Rice(چاول)', 
+ 'Barley(جو)', 
+ 'Soybean(سویابین)', 
+ 'Wheat(گندم)', 
+ 'Maize(مکئی)']
+)
 Rain_Fall = st.number_input("Enter the rain fall(in mm) in your fields")
 Temperature_Celsius = st.number_input("Enter your fields temperature(in celsius)")
 Fertilizer_Used = st.selectbox("Select Whether You Used Fertilizer(1 for Yes 0 for No):", [1,0])
@@ -58,12 +71,17 @@ Submit = st.button("Predict Yield")
 
 if Submit:
     Predictd_Yield = Model.predict(inputData)
-    st.write(f"Predicted Yield: {Predictd_Yield[0]:.2f} tons per hectare")
+    Predictd_Yield = Predictd_Yield * 1000 / 40 # since 1ton = 1000kgs & 1mann = 40kg => Converting into mannn per hactare. 
+    Predictd_Yield = Predictd_Yield / 2.471 # into mann per acre
+    st.write(f"Predicted Yield: {Predictd_Yield[0]:.2f} من پر ایکڑ") 
     import streamlit as st
 
     with open("metrics.pkl", "rb") as f:
         metrics = pickle.load(f)
 
+    col1, col2, col3 = st.columns([1,2,1])
+    with col2:
+        st.write("***Model Performances***")
     st.write(f"MSE: {metrics['MSE']:.2f}")
     st.write(f"MAE: {metrics['MAE']:.2f}")
     st.write(f"R2 Score: {metrics['R2']:.2f}")
@@ -98,7 +116,7 @@ if(recommendation):
             st.write("Bhai Sahb tm Irrigation use kro")
 
 
-    Crops = ['Cotton', 'Rice', 'Barley', 'Soybean', 'Wheat', 'Maize']
+    Crops = ['Cotton(روئی)',  'Rice(چاول)',  'Barley(جو)',  'Soybean(سویابین)',  'Wheat(گندم)',  'Maize(مکئی)']
 
     Actual_Crop = Crop
     Yields = []
@@ -112,8 +130,10 @@ if(recommendation):
     max_tuple = max(Yields, key=lambda x: x[1])
     crop_max = max_tuple[0]
     max_yield = max_tuple[1]
+    max_yield = max_yield * 1000 / 40 
+    max_yield = max_yield / 2.471
 
     if(crop_max != Actual_Crop):
-        st.write(f"Maximum possible yield with best crop: {max_yield:.2f} tons per hectare using {crop_max}")
+        st.write(f"Maximum possible yield with best crop: {max_yield:.2f} من پر ایکڑ using {crop_max}")
 
 
